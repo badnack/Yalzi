8e92d38a90d3b06881501fc51fb7ab82c223db83
Nilo Redini <n.redini@gmail.com>
2013-01-05 12:48:22 +0100

    added decompressor table and started decompression function

---
+++ src/LZ78decompressor.c
@@ -0,0 +1,80 @@
+#include "LZ78decompressor.h"
+#include <errno.h>
+
+dec_table*
+dec_table_create()
+{
+  dec_table* dt;
+  int i;
+
+  if((dt = (dec_table*) calloc(MAXNODES, sizeof(dec_table))) == NULL)
+    return NULL;
+
+  for(i = 0; i < FIRSTAVCHILD; i++){
+    dt[i].length = 1;
+    dt[i].f_label = ROOT;
+    
+    if((dt[i].word = malloc(1 * sizeof(uint8_t))) == NULL){
+      dec_table_destroy(dt);
+      free(dt);
+      return NULL;
+    }
+    dt[i].word[0] = (uint8_t)i;
+  }
+
+  return dt;
+}
+
+void
+dec_table_destroy(dec_table* dt)
+{
+  int n = MAXNODES;
+
+  if(dt == NULL)
+    return;
+  for(; n--;)
+    if(dt[n].word != 0)
+      free(dt[n].word);
+
+  memset(dt, 0, sizeof(dec_table) * MAXNODES);
+}
+
+int 
+decompress(BITIO* in_file, BITIO* out_file)
+{
+  int err_val;
+  uint32_t current_index, index_mask;
+  size_t index_length;
+  uint32_t c_label;
+  dec_table* dt;
+
+  if(in_file == NULL || out_file == NULL){
+    errno = EINVAL;
+    return -1;
+  }
+
+  if((dt = dec_table_create()) == NULL)
+    return -1;
+
+  index_length = FISTMASKLEN;
+  index_mask = (1 << index_length) - 1;
+  c_label = FIRSTAVCHILD;
+  err_val = 0;
+
+  while(1){
+    current_index = 0;
+
+    if(bitio_read(in_file, &current_index, index_length) < index_length || current_index > c_label){
+      err_val = -1;
+      break;
+    }
+
+    if(current_index == ROOT)
+      break;
+
+  }
+
+  dec_table_destroy(dt);
+
+  return err_val;
+}
---
+++ src/LZ78decompressor.h
@@ -0,0 +1,55 @@
+/**
+ * @file LZ78decompressor.h
+ *
+ * @author Nilo Redini
+ * @author Francesco Disperati
+ * @author Davide Pellegrino
+ * Variables and functions definitions for LZ78 decompression
+ *
+*/
+
+#ifndef _LZ78DECOMPRESSOR_H_
+#define _LZ78DECOMPRESSOR_H_
+
+#include "LZ78hashtable.h"
+#include "bitio.h"
+
+/** Decompressor table entry definition */
+typedef struct dec_table_entry dec_table_entry;
+/** Decompressor table definition */
+typedef struct dec_table_entry dec_table;
+
+/** Decompressor hash entry*/
+struct dec_table_entry{
+  /** Father label */
+  uint32_t f_label;
+  /** Word associated */
+  uint8_t* word;
+  /** Word length */
+  uint32_t length;
+};
+
+/**
+   Creates a decompressor table.
+
+   @return the table, or NULL if an error occurred
+*/
+dec_table* dec_table_create();
+
+/**
+   Decompress an LZ78 file.
+
+   @param in_file Input file
+   @param out_file Output file
+   @return zero on success. On error, -1 is returned, and errno is set appropriately.
+*/
+int decompress(BITIO* in_file, BITIO* out_file);
+
+/**
+   Destroys a decompressor table.
+
+   @param dt Table to destroy
+*/
+void dec_table_destroy(dec_table* dt);
+
+#endif
